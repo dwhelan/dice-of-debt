a966d0bb5e9628f1d638860adad720611a5f7f01
dwhelan <dwhelan@leanintuit.com>
2016-01-15 21:01:59 -0500

    Added initial iteration

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 module DiceOfDebt
   class Game
-    attr_accessor :score
+    attr_accessor :score, :iteration
 
     def initialize
-      self.score = 0
+      self.score     = 0
+      self.iteration = iterations.first
     end
 
     def roll
-      self.score += value_dice.roll - technical_debt_dice.roll
+      self.score += roll_value_dice - roll_technical_debt_dice
     end
 
     def config
@@ -21,12 +22,20 @@ module DiceOfDebt
       @value_dice ||= Dice.new(configuration.value_dice)
     end
 
+    def roll_value_dice
+      iteration.new_value = value_dice.roll
+    end
+
+    def roll_technical_debt_dice
+      iteration.technical_debt = technical_debt_dice.roll
+    end
+
     def technical_debt_dice
       @technical_debt_dice ||= Dice.new(configuration.technical_debt_dice)
     end
 
     def iterations
-      @iterations || Array.new(config.iterations) { Iteration.new }
+      @iterations ||= Array.new(config.iterations) { Iteration.new(0) }
     end
 
     class Dice
@@ -77,15 +86,32 @@ module DiceOfDebt
   end
 
   class Iteration
+    attr_accessor :initial_value, :new_value, :technical_debt
+
+    def initialize(initial_value)
+      self.initial_value  = initial_value
+      self.technical_debt = 0
+      self.new_value      = 0
+    end
+
+    def value
+      initial_value + new_value - technical_debt
+    end
   end
 end
 
 module DiceOfDebt
   describe Game do
-    let(:config) { subject.config }
-    let(:roller) { double('roller', roll: 1) }
+    let(:game)    { Game.new }
+    let(:subject) { game }
+    let(:config)  { game.config }
+    let(:roller)  { double('roller', roll: 1) }
 
-    before { config.value_dice.roller = config.technical_debt_dice.roller = roller }
+    before do
+      config.value_dice.roller         = config.technical_debt_dice.roller = roller
+      config.value_dice.count          = 8
+      config.technical_debt_dice.count = 4
+    end
 
     describe 'initially' do
       its(:score)              { should be 0 }
@@ -93,17 +119,31 @@ module DiceOfDebt
     end
 
     specify 'each roll of a value die should increase the score' do
-      config.value_dice.count = 1
       config.technical_debt_dice.count = 0
-      subject.roll
-      expect(subject.score).to eq 1
+      game.roll
+      expect(game.score).to eq 8
     end
 
     specify 'each roll of a technical debt die should decrease the score' do
       config.value_dice.count = 0
-      config.technical_debt_dice.count = 1
-      subject.roll
-      expect(subject.score).to eq (-1)
+      game.roll
+      expect(game.score).to eq (-4)
+    end
+
+    describe 'iteration' do
+      subject { game.iterations.first }
+
+      describe 'rolling value dice' do
+        before { game.roll_value_dice }
+        its(:new_value) { should be 8 }
+        its(:value)     { should be 8 }
+      end
+
+      describe 'rolling debt dice' do
+        before { game.roll_technical_debt_dice }
+        its(:technical_debt) { should be 4 }
+        its(:value)          { should be -4 }
+      end
     end
   end
 
@@ -115,23 +155,21 @@ module DiceOfDebt
     describe 'value_dice' do
       subject { Game.new.config.value_dice }
 
-      its(:count) { should be 8 }
-      its(:sides) { should be 6 }
+      its(:count)  { should be 8 }
+      its(:sides)  { should be 6 }
       its(:roller) { should be_a RandomRoller }
+
       its(:"roller.sides") { should be 6 }
     end
 
     describe 'technical_debt_dice' do
       subject { Game.new.config.technical_debt_dice }
 
-      its(:count) { should be 4 }
-      its(:sides) { should be 6 }
+      its(:count)  { should be 4 }
+      its(:sides)  { should be 6 }
       its(:roller) { should be_a RandomRoller }
+
       its(:"roller.sides") { should be 6 }
     end
-  end
-
-  describe 'iterations' do
-
   end
 end
