8a0afaa6003fa50c2cd91e919f068f5dd3c6b1f2
dwhelan <dwhelan@leanintuit.com>
2016-04-18 22:05:38 -0400

    Merged in some useful changes to reverted api/errors_spec

--- spec/api/errors_spec.rb
+++ spec/api/errors_spec.rb
@@ -9,18 +9,19 @@ module DiceOfDebt
 
       expect_error(404)
       expect(error['status']).to eq '404'
-      expect(error['title']).to  eq 'Invalid URI'
+      expect(error['title']).to eq 'Invalid URI'
       expect(error['detail']).to eq 'Invalid URI'
+      expect(error['source']).to be_nil
     end
 
     specify 'POST /errors' do
       post '/errors'
-      request_data = { data: { } }
-      post '/errors', request_data.to_json, {'CONTENT_TYPE' => 'application/vnd.api+json'}
+      request_data = { data: {} }
+      post '/errors', request_data.to_json, 'CONTENT_TYPE' => 'application/vnd.api+json'
 
       expect_error(500)
       expect(error['status']).to eq '500'
-      expect(error['title']).to  eq 'Internal Server Error'
+      expect(error['title']).to eq 'Internal Server Error'
       expect(error['detail']).to eq 'Internal Server Error'
       expect(error['source']).to be_nil
     end
