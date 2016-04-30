3d37c88d367b36876dcf07b84c0f5a67e3306c50
dwhelan <dwhelan@leanintuit.com>
2016-04-30 14:41:27 -0400

    Removed Iterations class

--- domain.rb
+++ domain.rb
@@ -1,12 +1,9 @@
 require_relative 'domain/die'
 require_relative 'domain/dice'
 require_relative 'domain/iteration'
-require_relative 'domain/iterations'
 require_relative 'domain/random_roller'
 require_relative 'domain/persistence'
 require_relative 'domain/game'
 
 # TODO: Assign ids via persister
-# TODO: Render via Grape:ROAR
 # TODO: Validate JSON - consider use of JSON:schema
-# TODO: return HAL results
