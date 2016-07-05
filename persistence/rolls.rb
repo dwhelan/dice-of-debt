9918c8cea26cc6c9273c53935c52f0861ede4db8
dwhelan <dwhelan@leanintuit.com>
2016-07-05 00:32:33 -0400

    Minor refactoring of repository classes

--- persistence/rolls.rb
+++ persistence/rolls.rb
@@ -13,19 +13,19 @@ module DiceOfDebt
     class RollRepository < Repository
       relations :rolls, :iterations, :games
 
+      def by_id(id)
+        roll = rolls.by_id(id).combine_parents(one: iterations).one
+        game = Persistence::ROM.game_repository.by_id(roll.iteration.game_id)
+        iteration = game.iterations.find { |i| i.id = roll.iteration_id }
+        iteration.roll
+      end
+
       def create(roll)
         roll.id = rolls.insert(iteration_id: roll.iteration.id, rolls: roll.rolls.to_json)
       end
 
       def update(roll)
         rolls.where(id: roll.id).update(rolls: roll.rolls.to_json)
-      end
-
-      def by_id(id)
-        roll = rolls.by_id(id).combine_parents(one: iterations).one
-        game = Persistence::ROM.game_repository.by_id(roll.iteration.game_id)
-        iteration = game.iterations.find { |i| i.id = roll.iteration_id }
-        iteration.roll
       end
     end
   end
