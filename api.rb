b4b201005ef7c0948bd10cdb016d214d3161b606
dwhelan <dwhelan@leanintuit.com>
2016-07-09 11:58:54 -0400

    Moved common helper methods to api file

--- api.rb
+++ api.rb
@@ -24,5 +24,37 @@ module DiceOfDebt
     route :any, '*path' do
       error(status: 404, title: 'Invalid URI')
     end
+
+    helpers do
+      def find_resource(type, id)
+        if valid_resource_id?(id)
+          error_for_invalid_resource_id(type, id)
+        else
+          Persistence::ROM.repository_for(type).by_id(id) || error_for_resource_not_found(type, id)
+        end
+      end
+
+      def valid_resource_id?(id)
+        id !~ /\d+/
+      end
+
+      def error_for_invalid_resource_id(type, id)
+        error(
+          status: 422,
+          title: "Invalid #{type} id",
+          detail: "The provided #{type} id '#{id}' should be numeric",
+          source: { parameter: :id }
+        )
+      end
+
+      def error_for_resource_not_found(type, id)
+        error(
+          status: 404,
+          title: "Could not find #{type}",
+          detail: "Could not find a #{type} with id #{id}",
+          source: { parameter: :id }
+        )
+      end
+    end
   end
 end
--- api/games.rb
+++ api/games.rb
@@ -1,5 +1,28 @@
 module DiceOfDebt
   class API
+    resource :games do
+      helpers do
+        def game_repository
+          Persistence::ROM.game_repository
+        end
+      end
+
+      post do
+        game = game_repository.create
+        header 'Location', "#{request.base_url}/games/#{game.id}"
+        GameRepresenter.as_document(game, request)
+      end
+
+      get do
+        GameSummaryRepresenter.as_document(game_repository.all, request)
+      end
+
+      get '/:id' do
+        game = find_resource('game', params[:id])
+        GameRepresenter.as_document(game, request) if game
+      end
+    end
+
     module BaseGameRepresenter
       # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
       def self.included(base)
@@ -32,67 +55,6 @@ module DiceOfDebt
 
       attributes do
         collection :iterations, extend: IterationRepresenter
-      end
-    end
-
-    helpers do
-      def find_game(id)
-        find_resource('game', id)
-      end
-
-      def find_resource(type, id)
-        if valid_resource_id?(id)
-          error_for_invalid_resource_id(type, id)
-        else
-          Persistence::ROM.repository_for(type).by_id(id) || error_for_resource_not_found(type, id)
-        end
-      end
-
-      def valid_resource_id?(id)
-        id !~ /\d+/
-      end
-
-      def error_for_invalid_resource_id(type, id)
-        error(
-          status: 422,
-          title: "Invalid #{type} id",
-          detail: "The provided #{type} id '#{id}' should be numeric",
-          source: { parameter: :id }
-        )
-      end
-
-      def error_for_resource_not_found(type, id)
-        error(
-          status: 404,
-          title: "Could not find #{type}",
-          detail: "Could not find a #{type} with id #{id}",
-          source: { parameter: :id }
-        )
-      end
-    end
-
-    resource :games do
-      helpers do
-        def repository
-          Persistence::ROM.game_repository
-        end
-      end
-
-      get do
-        GameSummaryRepresenter.as_document(repository.all, request)
-      end
-
-      post do
-        game = repository.create
-        header 'Location', "#{request.base_url}/games/#{game.id}"
-        GameRepresenter.as_document(game, request)
-      end
-
-      route_param :id do
-        get do
-          game = find_resource('game', params[:id])
-          GameRepresenter.as_document(game, request) if game
-        end
       end
     end
   end
